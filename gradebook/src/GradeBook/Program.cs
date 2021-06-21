67fc071667a47c9178818189e057367c0314d2b8
iainbrux <me@iainbrux.com>
2021-06-21 23:39:22 +0100

    first commit

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GradeBook
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double[] numbers = new double[3] { 12.22, 43.99, 9.33 };
+      double result = 0;
+      foreach(double number in numbers) {
+        result += number;
+      }
+
+      Console.WriteLine(result);
+    }
+  }
+}

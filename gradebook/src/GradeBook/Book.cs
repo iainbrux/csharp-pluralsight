10df814a19e69b1dc45d349615fbec63bc04cbe7
Iain Brookes <iain.brookes@outlook.com>
2021-06-24 10:22:48 +0100

    add Book class

---
+++ gradebook/src/GradeBook/Book.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+  class Book
+  {
+    public void AddGrade(double args) 
+    {
+      
+    }
+  }
+}
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,14 +8,18 @@ namespace GradeBook
     static void Main(string[] args)
     {
       double[] numbers = new double[3] { 12.22, 43.99, 9.33 };
-      List<double> grades  = new List<double>() { 12.22, 43.99, 9.33 };
+      List<double> grades = new List<double>() { 12.22, 43.99, 9.33 };
+
+      Book TotalGrades = new Book();
+      TotalGrades.AddGrade(32.3);
+
 
       grades.Add(16.1);
       grades.Add(77.3);
 
       double result = 0;
 
-      foreach(double number in grades) 
+      foreach (double number in grades)
       {
         result += number;
       }
